e30ded391c7e677eba01008b8cd6456d34d7bc94
Muhammad <muhammadsiddique14344@gmail.com>
2024-03-14 22:49:59 +0500

    initial commit

---
+++ Chapter 1/script.js
@@ -0,0 +1,14 @@
+//1.
+alert('Welcome to javascript');
+//2.;
+alert('Error! Please enter a valid password.');
+//3.
+var message = `Welcome to JS Land...
+Happy Coding!`;
+alert(message);
+//4.
+var newMessage = "Welcome to JS land...";
+alert(newMessage);
+//5.
+var last = "hello... I can run JS through my web browser's console";
+alert(last)
---
+++ chapter 2/script.js
@@ -0,0 +1,32 @@
+//1.
+var userName = 'jhone Doe'
+alert(userName);
+//2.
+var myName = " My name is Muhammad Siddique"
+alert(myName );
+//3.
+var message = "Hello World"
+alert(message);
+
+//4. 
+var studentName = "Jhone Doe";
+alert(studentName);
+var studentAge = "15 years old"
+alert(studentAge);
+var student = "Certified Mobile Application Development";
+alert(student);
+
+//6.
+var message = `PIZZA
+PIZZ
+PIZ
+PI
+P`
+alert(message);
+
+//7.
+var email = "example@gmail.com"
+alert(email);
+
+//8.
+document.write("HELLO WORLD")
